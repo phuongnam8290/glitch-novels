3c8ef5853869e61bdb97a582c064404029013ca0
phuongnam8290 <phuongnam8290@gmail.com>
2023-02-07 04:36:26 +0700

    🖥️feat: Scaffold backend modules
    - Create resources-server module.
    - Connect to db.

---
+++ glitch-novels-backend/resources-server/src/main/java/com/namdp/glitch_novels/resources_server/GlitchNovelsResourcesServerApplication.java
@@ -0,0 +1,13 @@
+package com.namdp.glitch_novels.resources_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlitchNovelsResourcesServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GlitchNovelsResourcesServerApplication.class, args);
+  }
+
+}
---
+++ glitch-novels-backend/resources-server/src/test/java/com/namdp/glitch_novels/resources_server/GlitchNovelsResourcesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namdp.glitch_novels.resources_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlitchNovelsResourcesServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
