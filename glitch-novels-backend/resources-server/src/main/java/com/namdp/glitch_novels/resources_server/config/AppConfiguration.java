dc36f8f426b2f1ffc3023799dff7ff7c5bee0391
phuongnam8290 <phuongnam8290@gmail.com>
2023-02-09 09:50:49 +0700

    🖥️feat(security): Add CORS for front-end access

---
+++ glitch-novels-backend/resources-server/src/main/java/com/namdp/glitch_novels/resources_server/config/AppConfiguration.java
@@ -0,0 +1,26 @@
+package com.namdp.glitch_novels.resources_server.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfiguration {
+
+  @Value("${cors.urll}")
+  private String[] corsUrl;
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins(corsUrl)
+            .allowedMethods("*");
+      }
+    };
+  }
+}
