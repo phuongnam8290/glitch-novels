b29fe0baec86da71e50e653200fe10701c751481
phuongnam8290 <phuongnam8290@gmail.com>
2023-04-16 11:49:00 +0700

    ️📱fix: Fix some minor bugs
    - DeleteButton now only shows when some items have been selected.
    - ConfirmModal shows custom messages signifying delete status when success/failure.
    - Cannot close ConfirmModal when modal's state is 'processing.'

--- glitch-novels-frontend/src/components/edit-mode/button/DeleteButton.vue
+++ glitch-novels-frontend/src/components/edit-mode/button/DeleteButton.vue
@@ -8,7 +8,7 @@
 
     <template #confirm-msg>
       Are you sure you want to delete these
-      <span class="font-bold"> {{ ARRAY_SELECTED_DATA.length }} </span>
+      <span class="font-bold"> {{ totalSelectedINovels }} </span>
       novels:
     </template>
 
@@ -24,13 +24,20 @@
     <template #confirm-warning-msg>
       <p>
         By deleting these
-        <span class="font-bold"> {{ ARRAY_SELECTED_DATA.length }} </span>
+        <span class="font-bold"> {{ totalSelectedINovels }} </span>
         novels,
         <span class="font-bold"> {{ numberOfChapters }} </span>
         chapters will also be delete.
       </p>
       <p class="mt-0 font-bold"> You cannot undo these action. </p>
     </template>
+
+    <template #success-msg>
+      <span class="font-bold"> {{ totalSelectedINovels }} </span>
+      novels was deleted successfully.
+    </template>
+
+    <template #failure-msg> Cannot delete selected novels. </template>
   </ConfirmableButton>
 </template>
 
@@ -42,6 +49,7 @@ import ConfirmableButton from "@/components/common/button/ConfirmableButton.vue"
 
 const editModeStore = useEditModeStore();
 const ARRAY_SELECTED_DATA = computed(() => editModeStore.ARRAY_SELECTED_DATA);
+const totalSelectedINovels = computed(() => ARRAY_SELECTED_DATA.value.length);
 const numberOfChapters = computed(() =>
   ARRAY_SELECTED_DATA.value.reduce((totalChapter, novel) => totalChapter + novel.chapters.length, 0)
 );
